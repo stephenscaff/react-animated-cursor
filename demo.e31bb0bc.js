f8408d49b8768d40ab01921cda9168d2362ec4eb
Stephen <smscaff@gmail.com>
2020-03-23 04:37:09 -0700

    Updates

--- demo.e31bb0bc.js
+++ demo.e31bb0bc.js
@@ -28747,7 +28747,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61606" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64237" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
